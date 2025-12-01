371f69f876d8907d3fdb91c1b48f79072b5bcbc3
John Doe <fosblmikkel@gmail.com>
2025-12-01 18:21:09 +0100

    fikset HTML links

--- app.js
+++ app.js
@@ -1,10 +1,40 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.json());
 
 // Middleware
 app.use(express.json());
+
+// HTML-routes
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'HTML', 'frontpage.html'));
+});
+
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'HTML', 'login.html'));
+});
+
+app.get('/createaccount', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'HTML', 'createaccount.html'));
+});
+
+app.get('/forgotpassword', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'HTML', 'forgotpassword.html'));
+});
+
+app.get('/howdoesbitchatwork', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'HTML', 'howdoesbitchatwork.html'));
+});
+
+app.get('/whatweoffer', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'HTML', 'whatweoffer.html'));
+});
+
+// Husk at slette denne inden aflevering
+app.get('/test', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'HTML', 'test.html'));
+});
+
 
 // Statisk public-mappe til frontend filer
 app.use(express.static(path.join(__dirname, 'public')));
@@ -18,5 +48,4 @@ const port = 3000;
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
-
 
